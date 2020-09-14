514ad472f51e9633463c0ab569ed0a44678e56b3
NganVanLuyen <nganvanluyen@gmail.com>
2020-09-14 09:36:53 +0700

    "tom eat jerry"

--- cat.js
+++ cat.js
@@ -1,9 +1,16 @@
+var Mouse = require('./mouse')
+
 function Cat(name) {
 	this.name = name;
 	this.stomach = [];
 };
 
-Cat.prototype.eat = function (Mouse) {
+Cat.prototype.eat = function (animal) {
+	if(animal instanceof Mouse) {
 	this.stomach.push(Mouse);
+	} else{
+		throw new Error('Cat can only eat Mouse!');
+	}
 };
+
 module.exports = Cat;
